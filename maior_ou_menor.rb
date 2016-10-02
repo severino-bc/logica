e97e98514547fde894ad03610c306b38ae238812
Severino <severinosilva.bc@gmail.com>
2016-10-02 20:33:51 -0300

    Adicionado parâmetros para interação com o usuário

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,5 +1,23 @@
 puts "Bem vindo ao jogo da adivinhação"
-
+puts
+puts
+puts
 puts "Qual seu nome? "
 nome = gets
 puts"Começaremos o jogo para você, " + nome
+puts
+puts
+
+numero_secreto = 175
+
+puts"Escolhendo um némero secreto em 0 e 200..."
+puts"Escolhido... que tal adivinhar hoje nosso número secreto? "
+
+puts "Tentativa 1"
+puts
+puts
+puts"Entre com um número"
+chute = gets
+puts
+
+puts"Você chutou o #{chute}"
