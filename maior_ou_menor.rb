d7f2f20297520e97870359d91f089cc072dec484
Severino Manoel <severino@new-host.home>
2017-07-31 06:08:08 -0300

    Def

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,23 +1,45 @@
-puts "Bem vindo ao jogo da adivinhação"
-puts
-puts
-puts
-puts "Qual seu nome? "
-nome = gets
-puts"Começaremos o jogo para você, " + nome
-puts
-puts
+def da_boa_vindas
+	puts "Bem vindo ao jogo da adivinhação"
+	puts "\n\n"
+	puts "Qual seu nome? "
+	nome = gets
+	puts"Começaremos o jogo para você, " + nome
+end
+
+def sortea_numero_secreto
+	puts "Tentativa " + tentativa.to_s + " de " + limite_tentativa.to_s
+	puts"Entre com um número"
+	chute = gets
+	puts
+	puts"Será que acertou? você chutou : #{chute}"
+	sleep(2)
+end
 
 numero_secreto = 175
+da_boa_vindas()
+
 
 puts"Escolhendo um némero secreto em 0 e 200..."
 puts"Escolhido... que tal adivinhar hoje nosso número secreto? "
+puts "\n\n"
+	
+limite_tentativa = 5
 
-puts "Tentativa 1"
-puts
-puts
-puts"Entre com um número"
-chute = gets
-puts
+for tentativa in 1..limite_tentativa
 
-puts"Será que você acertou? seu chutou foi: #{chute}"
+	acertou = chute.to_i == numero_secreto
+	maior = chute.to_i < numero_secreto
+
+	if acertou
+		puts"Parabéns você certou nosso numero secreto!"
+	else
+		if maior	
+			puts"O número secreto é maior"
+		else
+		    puts"O número secreto é menor"	
+			
+		end
+	    puts"Você não acertou nosso número secreto"	
+
+	end
+end	    
